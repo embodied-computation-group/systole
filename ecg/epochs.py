79a4ecb10defcdce3a4767e70195e774ed263089
Nicolas Marc Simon Legrand <au646069@uni.au.dk>
2019-11-12 20:15:19 +0100

    * epochs.epochs - control for vector lenght - rename to 'to_epochs' - add apply_baseline and event_idx parameters

--- ecg/epochs.py
+++ ecg/epochs.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from scipy.ndimage import gaussian_filter1d
 
 
-def epochs(x, events, sfreq, tmin=-1, tmax=10):
-    """Epoch signal.
+def to_epochs(x, events, sfreq, tmin=-1, tmax=10, event_idx=1, smooth=True,
+              sigma=10, apply_baseline=0):
+    """Epoch signal based on events indexes.
 
     Parameters
     ----------
@@ -18,22 +20,43 @@ def epochs(x, events, sfreq, tmin=-1, tmax=10):
         Start time before event, in seconds.
     tmax : float, defautl to 10
         End time after event, in seconds.
+    event_idx : int
+        The index of event of interest. Default is `1`.
+    apply_baseline : int, tuple, None
+        If int or tuple, use the point or interval to apply a baseline (method:
+        mean). If None, no baseline is applied.
 
     Returns
     -------
     epochs : ndarray
-        The epoched signal.
-
-    Examples
-    --------
+        Event * Time array.
     """
-    if len(events) == x.shape[0]:
-        # From boolean to event indexes
-        events = np.where(events == 1)[0]
+    # From boolean to event indexes
+    events = np.where(events == event_idx)[0]
 
-    epochs = []
-    for i in events:
-        trial = x[i+round(tmin*sfreq):i+round(tmax*sfreq)]
-        epochs.append(trial)
+    if smooth:
+        x = gaussian_filter1d(x, sigma=sigma)
 
-    return np.asarray(epochs)
+    rejected = 0
+    epochs = np.zeros(
+                shape=(len(events), ((np.abs(tmin) + np.abs(tmax)) * sfreq)))
+    for i, ev in enumerate(events):
+        if (ev+round(tmax*sfreq)) < len(x):
+            trial = x[ev+round(tmin*sfreq):ev+round(tmax*sfreq)]
+            if apply_baseline is None:
+                epochs[i, :] = trial
+            else:
+                if isinstance(apply_baseline, int):
+                    baseline = x[ev+round(apply_baseline*sfreq)]
+                if isinstance(apply_baseline, tuple):
+                    low = ev+round(apply_baseline[0]*sfreq)
+                    high = ev+round(apply_baseline[1]*sfreq)
+                    baseline = x[low:high].mean()
+                epochs[i, :] = trial - baseline
+        else:
+            rejected += 1
+
+    if rejected > 0:
+        print(str(rejected) + ' trial(s) droped due to inconsistent recording')
+
+    return epochs
