51d4b41ae074476dd32856a22fb741141e750320
Nicolas Marc Simon Legrand <au646069@uni.au.dk>
2019-11-26 19:12:42 +0100

    Add hrv_nonlinear - SD1, SD2

---
+++ systole/hrv_nonlinear.py
@@ -0,0 +1,31 @@
+# Author: Nicolas Legrand <nicolas.legrand@cfin.au.dk>
+
+import numpy as np
+import pandas as pd
+
+
+def nonlinear_domain(x):
+    """Extract the frequency domain features of heart rate variability.
+
+    Parameters
+    ----------
+    x : list or numpy array
+        Length of R-R intervals (in miliseconds).
+
+    Returns
+    -------
+    stats : pandas DataFrame
+        DataFrame of HRV parameters (frequency domain)
+    """
+
+    diff_rr = np.diff(x)
+    sd1 = np.sqrt(np.std(diff_rr, ddof=1) ** 2 * 0.5)
+    sd2 = np.sqrt(2 * np.std(x, ddof=1) ** 2 - 0.5 * np.std(diff_rr,
+                                                            ddof=1) ** 2)
+    values = [sd1, sd2]
+    metrics = ['SD1', 'SD2']
+
+    stats = pd.DataFrame({'Values': values, 'Metric': metrics}),
+                         ignore_index=True, sort=False)
+
+    return stats
