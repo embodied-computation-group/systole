34597674fdf4eb8bbf4b50333a25f8057b4ef1ae
Nicolas Marc Simon Legrand <au646069@uni.au.dk>
2019-12-01 18:55:57 +0100

    Readme: add hrv results

--- systole/__init__.py
+++ systole/__init__.py
@@ -3,9 +3,7 @@ from .detection import *
 from .epochs import *
 from .utils import *
 from .plotting import *
-from .hrv_time import *
-from .hrv_frequency import *
-from .hrv_nonlinear import *
+from .hrv import *
 from .datasets import *
 
 __version__ = "0.0.1a"
