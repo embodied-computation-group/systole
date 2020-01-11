c3ade7ac93a33cc1d7415906ebb280c7795de429
Nicolas Marc Simon Legrand <au646069@uni.au.dk>
2020-01-11 19:04:28 +0100

    remove deleted module import

--- systole/__init__.py
+++ systole/__init__.py
@@ -1,9 +1,7 @@
-from .circular import *
 from .detection import *
-from .epochs import *
 from .utils import *
 from .plotting import *
 from .hrv import *
 from .datasets import *
 
-__version__ = "0.0.1a"
+__version__ = "0.0.1"
