430edd39b27eba53a6a8b352f10b7b50c0c0fd44
lbanellis <leahbanellis@gmail.com>
2022-09-06 14:59:28 +0200

    save signals, un/corrected peaks in DataFrame

--- systole/io.py
+++ systole/io.py
@@ -1,17 +1,20 @@
 # Author:  Leah Banellis <leahbanellis@cfin.au.dk>
-import numpy as np
+import json
+from pathlib import Path
+
+import pandas as pd
 
 
 def import_manual_correction(
-    peaks_ms: np.ndarray, participant_id: str, session: str, modality: str, pattern: str
-) -> np.ndarray:
+    bids_path: str, participant_id: str, session: str, modality: str, pattern: str
+) -> pd.DataFrame:
 
-    """Correct extra and missed peaks identified via manual correction (i.e., using .json file via the systole viewer)
+    """Correct extra and missed peaks identified via manual correction (i.e., using saved .json file via the systole viewer)
 
     Parameters
     ----------
-    peaks_ms : np.ndarray
-            1d array of RR intervals, or  interbeat intervals (IBI), expressed in milliseconds.
+    bids_path : str
+        path of bids folder (i.e., "mnt/scratch/BIDS")
     participant_id : str
         participant id (i.e., "sub-0001").
     session : str
@@ -23,25 +26,64 @@ def import_manual_correction(
 
     Returns
     -------
-    peaks_ms_corrected : np.ndarray
-        The corrected RR time series
+    ppg_df : pd.DataFrame
+        DataFrame of raw cardiac signal, preprocessed cardiac signal, uncorrected peaks and corrected peaks.
     """
 
-    import json
-    from os.path import exists
+    # load raw physio signal
+    raw_file = Path(
+        f"{bids_path}/{participant_id}/{session}/{modality}/{participant_id}_{session}_{pattern}_physio.tsv.gz"
+    )
+    signal_raw = pd.read_csv(
+        raw_file,
+        compression="gzip",
+        sep="\t",
+        header=None,
+    )
 
-    corrected_path = "/mnt/scratch/BIDS/derivatives/systole/corrected/"
-    corrected_json = f"{corrected_path}{participant_id}/{session}/{modality}/sub-{participant_id}_{session}_{pattern}_corrected.json"
+    # load preprocessed signal
+    preproc_file = Path(
+        f"{bids_path}/derivatives/systole/{participant_id}/{session}/{modality}/{participant_id}_{session}_{pattern}_physio.tsv.gz"
+    )
+    signal_preproc = pd.read_csv(
+        preproc_file,
+        compression="gzip",
+        sep="\t",
+        header=None,
+    )
 
-    # if corrected peaks file exists
-    if exists(corrected_json):
-        f = open(f"{corrected_json}")
+    # json with manual corrections
+    corrected_json_path = Path(
+        f"{bids_path}/derivatives/systole/corrected/{participant_id}/{session}/{modality}/sub-{participant_id}_{session}_{pattern}_corrected.json"
+    )
+
+    # if corrected peaks json file exists
+    if corrected_json_path.is_file():
+        f = open(f"{corrected_json_path}")
         json_dict = json.load(f)
 
+        peaks_uncorrected = json_dict["ppg"]["peaks"]
+
+        peaks_corrected = peaks_uncorrected
+
         # add manually selected peaks
-        peaks_ms[json_dict["ppg"]["add_idx"]] = True
+        peaks_corrected[json_dict["ppg"]["add_idx"]] = True
 
         # remove manually selected peaks
-        peaks_ms[json_dict["ppg"]["remove_idx"]] = False
+        peaks_corrected[json_dict["ppg"]["remove_idx"]] = False
+
+        # save all
+        ppg_df = pd.DataFrame(
+            {
+                "raw_signal": signal_raw,
+                "preproc_signal": signal_preproc,
+                "peaks_uncorrected": peaks_uncorrected,
+                "peaks_corrected": peaks_corrected,
+            }
+        )
+        ppg_df.to_csv(
+            f"{bids_path}/derivatives/systole/corrected/{participant_id}/{session}/{modality}/sub-{participant_id}_{session}_{pattern}_corrected.tsv",
+            sep="\t",
+        )
 
     return
