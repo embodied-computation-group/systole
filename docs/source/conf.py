6db505ea8b20fbc78311f2680e5fb8c2a8dc0178
Nicolas Legrand <nicolas.legrand@cfin.au.dk>
2023-04-11 16:37:52 +0200

    Fix git actions for tests and code coverage (#55)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,6 +64,10 @@ panels_add_bootstrap_css = False
 autosummary_generate = True
 numpydoc_show_class_members = False
 
+# raise an error if the documentation does not build and exit the process
+# this should especially ensure that the notebooks run correctly
+nb_execution_raise_on_error = True
+
 # Include the example source for plots in API docs
 plot_include_source = True
 plot_formats = [("png", 90)]
